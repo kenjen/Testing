e00c029f5b44bd7c85ed186be30355d385c1ee5e
Kian Jennings <kianjennings@gmail.com>
2015-02-16 15:22:45 +0000

    Update Main.java

--- Testing/src/main/java/main/Main.java
+++ Testing/src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Working :)");
+		System.out.println("Working B");
 		// extra line added in browser to test pull
 	}
 

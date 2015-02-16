9d99dac44718bee7406f4ed242b946140bb1bd6d
kian jennings <kianjennings@gmail.com>
2015-02-16 15:24:03 +0000

    added conflict

--- Testing/src/main/java/main/Main.java
+++ Testing/src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Working :)");
+		System.out.println("Working A");
 		// extra line added in browser to test pull
 	}
 
